fe20d9b353220a1e11d95fdc28a32f3fc6e76c88
Carlos Rodrigues <carlosrodriguesjf@gmail.com>
2023-05-21 14:42:22 -0300

    Versão final raiway v2

--- projeto_meus_filmes/settings.py
+++ projeto_meus_filmes/settings.py
@@ -133,11 +133,3 @@ CRISPY_ALLOWED_TEMPLATE_PACKS ='bootstrap5'
 CRISPY_TEMPLATE_PACK ='bootstrap5'
 LOGIN_REDIRECT_URL = 'pagina_inicial'
 LOGIN_URL ='login'
-
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
